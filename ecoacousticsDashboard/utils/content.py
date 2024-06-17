1fa5d4b6c4c51e3d933f757546584d41de3218e1
LukasKG <lukasgue@gmail.com>
2024-06-17 15:38:27 +0100

    Add content to docker env and capture content file not found error

--- ecoacousticsDashboard/utils/content.py
+++ ecoacousticsDashboard/utils/content.py
@@ -1,17 +1,27 @@
+import os
 from pathlib import Path
+
 from dash import html
+from loguru import logger
 
 CONTENT_PATH = Path(Path.cwd(), 'content')
 def get_content_file(name):
-    return Path(CONTENT_PATH, name)
+    file_path = Path(CONTENT_PATH, name)
+    if not os.path.isfile(file_path):
+        logger.warning(f"Cannot find \'{file_path}\'")
+        return None
+    return file_path
 
 def get_content_text(name):
-    content_path = get_content_file(name)
+    file_path = get_content_file(name)
+
+    if file_path is None:
+        return ""
 
     content = []
     text = ""
 
-    with open(content_path) as this_file:
+    with open(file_path) as this_file:
         for a in this_file.read():
             if "\n" in a:
                 content.append(text)
