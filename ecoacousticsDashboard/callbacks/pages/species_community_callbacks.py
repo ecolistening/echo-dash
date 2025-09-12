e2b895b419eecf0393f1a4ef2e69e7f0fc8020db
Kieran Gibb <kg@magmacollective.org>
2025-09-12 15:30:23 +0100

    refine the species matrix, show species by functional group (row)

--- ecoacousticsDashboard/callbacks/pages/species_community_callbacks.py
+++ ecoacousticsDashboard/callbacks/pages/species_community_callbacks.py
@@ -18,8 +18,7 @@ from api import dispatch, FETCH_BIRDNET_SPECIES
 from utils import list2tuple, send_download
 
 MAX_WIDTH = 1600
-CELL_WIDTH = 50
-CELL_HEIGHT = 25
+CELL_HEIGHT = 40
 
 def fetch_data(dataset_name, filters):
     return dispatch(
@@ -54,69 +53,99 @@ def plot(
         counts["_col_facet"] = "All"
         facet_col = "_col_facet"
 
-    row_categories = category_orders.get(facet_row, counts[facet_row].unique())
-    col_categories = category_orders.get(facet_col, counts[facet_col].unique())
-    categories = list(itertools.product(row_categories, col_categories))
-
     z_min = 0
     z_max = max(counts["detected"].max(), 1)
     all_x = counts[axis_group].unique().tolist()
-    all_y = counts["species"].unique().tolist()
+
+    # calculate plot heights proportional to the number of species detected
+    row_categories = []
+    row_heights = []
+    for row_cat in category_orders.get(facet_row, counts[facet_row].unique()):
+        species_count = counts.loc[counts[facet_row] == row_cat, "species"].count()
+        if species_count > 0:
+            row_categories.append(row_cat)
+            row_heights.append(species_count)
+    row_heights = np.array(row_heights)
+    height = CELL_HEIGHT * row_heights.sum()
+    row_heights = list(row_heights / row_heights.sum())
+
+    # col categories remain consistent width
+    col_categories = category_orders.get(facet_col, counts[facet_col].unique())
 
     fig = make_subplots(
         rows=len(row_categories),
         cols=len(col_categories),
+        row_heights=None if len(row_heights) == 1 else list(row_heights),
         subplot_titles=[
-            f"{r if r != 'All' else ''} {c if c != 'All' else ''}".strip()
+            "<br>".join(list(filter(None, [
+                str(r) if r != 'All' else None,
+                str(c) if c != 'All' else None
+            ])))
             for r in row_categories
             for c in col_categories
         ],
-        shared_yaxes=True,
         horizontal_spacing=0.01,
-        vertical_spacing=0.01,
+        vertical_spacing=0.075,
     )
 
-    for i, (row_cat, col_cat) in enumerate(categories):
-        row = i // len(col_categories) + 1
-        col = i % len(col_categories) + 1
-        subset = counts[(counts[facet_row] == row_cat) & (counts[facet_col] == col_cat)]
-        if subset.empty:
-            subset = pd.DataFrame(0, index=all_y, columns=all_x)
-        else:
-            subset = subset.pivot_table(
-                index="species",
-                columns=axis_group,
-                values="detected",
-                aggfunc="sum",
-                fill_value=0,
-            ).reindex(
-                index=all_y,
-                columns=all_x,
-                fill_value=0,
+    for i, row_cat in enumerate(row_categories):
+        row = i + 1
+        row_species = counts.loc[counts[facet_row] == row_cat, "species"]
+        for j, col_cat in enumerate(col_categories):
+            col = j + 1
+            subset = counts[(counts[facet_row] == row_cat) & (counts[facet_col] == col_cat)]
+            if subset.empty:
+                subset = pd.DataFrame(0, index=row_species, columns=all_x)
+            else:
+                subset = subset.pivot_table(
+                    index="species",
+                    columns=axis_group,
+                    values="detected",
+                    aggfunc="sum",
+                    fill_value=0,
+                ).reindex(
+                    index=row_species,
+                    columns=all_x,
+                    fill_value=0,
+                )
+            fig.add_trace(
+                go.Heatmap(
+                    z=subset.values,
+                    x=subset.columns,
+                    y=row_species,
+                    colorscale="Greys",
+                    text=subset.values,
+                    texttemplate="%{text}",
+                    textfont={"size": 10, "color": "black"},
+                    showscale=(row == 1 and col == len(col_categories)),
+                    zmin=z_min,
+                    zmax=z_max,
+                ),
+                row=row, col=col,
             )
-        fig.add_trace(
-            go.Heatmap(
-                z=subset.values,
-                x=subset.columns,
-                y=subset.index,
-                colorscale="Greys",
-                text=subset.values,
-                texttemplate="%{text}",
-                textfont={"size": 10, "color": "black"},
-                showscale=(row == 1 and col == len(col_categories)),
-                zmin=z_min,
-                zmax=z_max,
-            ),
-            row=row, col=col,
-        )
-    fig.update_yaxes(
-        categoryorder="array",
-        categoryarray=all_y,
-    )
+
+    updates = {}
+    for i in range(len(row_categories)):
+        for j in range(2, len(col_categories) + 1):
+            axis_num = i * len(col_categories) + j
+            updates[f"yaxis{axis_num}"] = dict(showticklabels=False)
+    # for i in range(len(row_categories)):
+    #     base_axis_num = i * len(col_categories) + 1  # first col in row i
+    #     base_axis = "y" + (str(base_axis_num) if base_axis_num > 1 else "")  # y, y2, y3...
+    #     for j in range(2, len(col_categories)+1):  # rest of cols in the row
+    #         axis_num = i * len(col_categories) + j
+    #         axis_name = "yaxis" + str(axis_num)  # this is the layout property
+    #         match_name = "y" + (str(base_axis_num) if base_axis_num > 1 else "")  # this is the value for 'matches'
+    #         updates[axis_name] = {"matches": match_name}
+    # fig.update_yaxes(
+    #     categoryorder="array",
+    #     categoryarray=all_y,
+    # )
     fig.update_layout(
         barmode='stack',
-        height=CELL_HEIGHT * len(all_y) * len(row_categories),
+        height=height,
         width=MAX_WIDTH,
+        **updates,
         title=dict(
             automargin=True,
             x=0.5,
