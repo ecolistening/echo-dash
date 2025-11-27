7b754e8d3e33e53f320ac1e403f3579f231fba5b
Kieran Gibb <kg@magmacollective.org>
2025-11-27 11:45:52 +0000

    add kilpis to shared umap script

--- src/scripts/umap.sh
+++ src/scripts/umap.sh
@@ -4,3 +4,4 @@ uv run scripts/train_umap.py --acoustic-features-path=../data/cairngorms/recordi
 uv run scripts/train_umap.py --acoustic-features-path=../data/nature_sense/recording_acoustic_features_table.parquet --save-dir=../data/nature_sense/umap/
 uv run scripts/train_umap.py --acoustic-features-path=../data/sounding_out/recording_acoustic_features_table.parquet --save-dir=../data/sounding_out/umap/
 uv run scripts/train_umap.py --acoustic-features-path=../data/sounding_out_chorus/recording_acoustic_features_table.parquet --save-dir=../data/sounding_out_chorus/umap/
+uv run scripts/train_umap.py --acoustic-features-path=../data/kilpisjarvi/recording_acoustic_features_table.parquet --save-dir=../data/kilpisjarvi/umap/
